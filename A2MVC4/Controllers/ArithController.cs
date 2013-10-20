6ff097541583bec5e1ae607f57d42a7c0cf3bfc1
Raoul <raoul_dcunha@hotmail.com>
2013-10-20 20:20:25 +1300

    created ArithController

---
+++ A2MVC4/Controllers/ArithController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace A2MVC4.Controllers
+{
+    public class ArithController : Controller
+    {
+        //
+        // GET: /Arith/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
